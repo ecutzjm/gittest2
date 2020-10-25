1633f2e650d7e1d964e6f6a3d08b6f96074898fa
ecutzjm <1062377385@qq.com>
2020-10-25 23:37:33 +0800

    helloGit master second

---
+++ src/main/java/com/atguigu/HelloGit2.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+public class HelloGit2 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello master");
+    }
+}
